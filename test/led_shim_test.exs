fec4a83079245c8c8a3eed3c78da55216e43511e
David Kormushoff <kormie@gmail.com>
2018-11-27 00:15:51 -0800

    Add test for valid pixel

--- test/led_shim_test.exs
+++ test/led_shim_test.exs
@@ -2,7 +2,13 @@ defmodule LEDShimTest do
   use ExUnit.Case
   doctest LEDShim
 
-  test "greets the world" do
-    assert LEDShim.hello() == :world
+  test "errors when LED pixel is out of bounds" do
+    LEDShim.start_link
+    assert {:error, _message} = LEDShim.set_pixel(28, {255, 255, 255}, 0.2)
+  end
+
+  test "accepts pixels between 0 and 27" do
+    LEDShim.start_link
+    for i <- 0..27, do: assert :ok = LEDShim.set_pixel(i, {255, 255, 255}, 0.2)
   end
 end
